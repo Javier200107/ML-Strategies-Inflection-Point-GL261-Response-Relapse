bc939dd8b3905466c2d71f5d0ce21fb180072b33
Javier200107 <xaviergonzalezbejar@gmail.com>
2024-12-26 01:05:31 +0100

    Deep learning part

--- tfrecordimagehandler.py
+++ tfrecordimagehandler.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 class TFRecordImageHandler:
-    def __init__(self, tfrecord_file, batch_size=32, shuffle=True, augment=False):
+    def __init__(self, tfrecord_file, batch_size=32, shuffle=False, augment=False):
         self.tfrecord_file = tfrecord_file
         self.batch_size = batch_size
         self.shuffle = shuffle
@@ -11,7 +11,7 @@ class TFRecordImageHandler:
         self.length = self.__len__()
 
     def _parse_function(self, proto):
-        # Definir la descripción de las características
+        # Define the description of the features
         feature_description = {
             'image': tf.io.FixedLenFeature([], tf.string),
             'height': tf.io.FixedLenFeature([], tf.int64),
@@ -25,76 +25,58 @@ class TFRecordImageHandler:
         }
         parsed_features = tf.io.parse_single_example(proto, feature_description)
 
-        
-        # Decodificar la imagen
+        # Decode the image
         image = tf.io.decode_raw(parsed_features['image'], tf.uint8)
         image = tf.reshape(image, [parsed_features['height'], parsed_features['width'], 3])
         image = tf.image.rgb_to_grayscale(image)
-        # Redimensionar la imagen a un tamaño uniforme (por ejemplo, 256x256)
         image = tf.image.resize(image, [256, 256])
 
-        # Decodificar la máscara
+        # Decode the mask
         mask = tf.io.decode_raw(parsed_features['mask'], tf.uint8)
         mask = tf.reshape(mask, [parsed_features['mask_height'], parsed_features['mask_width'], 1])
-
-        # Redimensionar la máscara a un tamaño uniforme (por ejemplo, 256x256)
         mask = tf.image.resize(mask, [256, 256])
-
-        self._normalize(image, mask)
 
         # Combine image and mask into a 2-channel image
         image = tf.concat([image, mask], axis=-1)
 
-        # Convertir el group_name a one-hot encoding
-        group_name_one_hot = tf.one_hot(parsed_features['group_name'], depth=3)  # 3 es el número de clases
+        # Cast group_name to float
+        group_name = tf.cast(parsed_features['group_name'], tf.float32)
         
-        # return image, mask, parsed_features['group_name'], parsed_features['mouse_id'], parsed_features['day_of_study']
-        return image, group_name_one_hot
+        return image, group_name
 
-    # def _normalize(self, image, group_name): # self, image, mask, group_name, mouse_id, day_of_study
-    #     # Convertir la imagen a float y normalizarla al rango [0, 1]
-    #     image = tf.cast(image, tf.float32) / 255.0
-    #     # Convertir la máscara a float y normalizarla al rango [0, 1]
-    #     #mask = tf.cast(mask, tf.float32) / 255.0
-        
-    #     #return image, mask, group_name, mouse_id, day_of_study
-    #     return image, group_name
-
-    def _normalize(self, image, mask):
-        # Convertir la imagen a float y normalizarla al rango [0, 1]
+    def _normalize(self, image):
+        # Normalize image to [0, 1] range
         image = tf.cast(image, tf.float32) / 255.0
-        # Convertir la máscara a float y normalizarla al rango [0, 1]
-        mask = tf.cast(mask, tf.float32) / 255.0
-        return image, mask
+        return image
 
-    def _augment(self, image, group_name): # self, image, mask, group_name, mouse_id, day_of_study
-        # Aumentación: flipping horizontal y brillo aleatorio
+    def _augment(self, image, group_name):
+        # Apply augmentation: horizontal flipping and random brightness
         if tf.random.uniform(()) > 0.5:
             image = tf.image.flip_left_right(image)
-            #mask = tf.image.flip_left_right(mask)
         image = tf.image.random_brightness(image, max_delta=0.1)
-
-        #return image, mask, group_name, mouse_id, day_of_study
         return image, group_name
 
     def _load_dataset(self):
-        # Cargar el archivo TFRecord y aplicar los mapeos
+        # Load the TFRecord file and apply mappings
         dataset = tf.data.TFRecordDataset(self.tfrecord_file)
         dataset = dataset.map(self._parse_function, num_parallel_calls=tf.data.AUTOTUNE)
-        #dataset = dataset.map(self._normalize, num_parallel_calls=tf.data.AUTOTUNE)
+
+        # Normalize before augmenting
+        dataset = dataset.map(lambda img, lbl: (self._normalize(img), lbl), num_parallel_calls=tf.data.AUTOTUNE)
 
         if self.augment:
             dataset = dataset.map(self._augment, num_parallel_calls=tf.data.AUTOTUNE)
-
+        
         if self.shuffle:
             dataset = dataset.shuffle(buffer_size=1000)
 
-        # Aplicar batch y prefetch para eficiencia
+        # Apply batching and prefetching for efficiency
         dataset = dataset.batch(self.batch_size)
         dataset = dataset.prefetch(tf.data.AUTOTUNE)
         
         return dataset
-    
+
     def __len__(self):
-        size = sum(1 for _ in tf.data.TFRecordDataset(self.tfrecord_file))
+        # Use cardinality to determine dataset size
+        size = tf.data.TFRecordDataset(self.tfrecord_file).cardinality().numpy()
         return int(np.ceil(size / self.batch_size))
